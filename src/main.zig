571ceaa08cceb4cc7a02d9f52b8ba7b91093d093
GoldenStack <git@goldenstack.net>
2025-02-07 23:50:35 -0600

    Add a game struct

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
 const buffer = @import("buffer.zig");
 const tty = @import("platform/tty.zig");
+const Game = @import("game.zig").Game;
 
 const posix = std.posix;
 const termios = std.posix.termios;
@@ -9,24 +10,22 @@ pub fn main() !void {
     const term = try tty.Tty.init();
     defer term.deinit();
 
-    var x: usize = 0;
-    var y: usize = 0;
+    var game = Game.init();
 
+    try term.render(&game.compose());
     while (true) {
         const key = try term.blocking_input();
         const slice = key.slice();
 
         switch (slice[0]) {
-            'w' => { if (y > 0) y -= 1; },
-            'a' => { if (x > 0) x -= 1; },
-            's' => { if (y < buffer.Height - 1) y += 1; },
-            'd' => { if (x < buffer.Width - 1) x += 1; },
+            'w' => game.move_player(0, -1),
+            'a' => game.move_player(-1, 0),
+            's' => game.move_player(0, 1),
+            'd' => game.move_player(1, 0),
             else => {},
         }
 
-        var buf = buffer.Buffer.init();
-        buf.set(y, x, 'X');
-        try term.render(&buf);
+        try term.render(&game.compose());
 
         if (slice[0] == 13) {
             return;
