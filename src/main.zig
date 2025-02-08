bf6ded75a47786839bf8255fefc61a030b8222c1
GoldenStack <git@goldenstack.net>
2025-02-07 20:35:01 -0600

    Implement basic movement

--- src/main.zig
+++ src/main.zig
@@ -9,14 +9,24 @@ pub fn main() !void {
     const term = try tty.Tty.init();
     defer term.deinit();
 
-    var buf = buffer.Buffer.init();
-    buf.set(5, 1, 'X');
+    var x: usize = 0;
+    var y: usize = 0;
 
     while (true) {
         const key = try term.blocking_input();
         const slice = key.slice();
 
-        try term.render(&buffer.Buffer.fill(slice[0]));
+        switch (slice[0]) {
+            'w' => { if (y > 0) y -= 1; },
+            'a' => { if (x > 0) x -= 1; },
+            's' => { if (y < buffer.Height - 1) y += 1; },
+            'd' => { if (x < buffer.Width - 1) x += 1; },
+            else => {},
+        }
+
+        var buf = buffer.Buffer.init();
+        buf.set(y, x, 'X');
+        try term.render(&buf);
 
         if (slice[0] == 13) {
             return;
